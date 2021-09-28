da80077fe28130c4a52846ff9034bcc2bc20d127
Erico103 <85289051+Erico103@users.noreply.github.com>
2021-09-28 13:47:14 +0200

    Delete todo.js

--- todo.js
+++
@@ -1,3 +0,0 @@
-export const _ = [ { '_id': `23cfa1d1-7ed0-4355-bcda-a4a57b53bdb3`, '_createdOn': `2021-09-22T14:25:52.879Z` } ];
-_[1] = { 'description': `buy oatmeal`, 'done': `false`, '_id': `b848bf70-f951-48e4-b81f-b423d39a7d4a`, '_createdOn': `2021-09-22T15:12:32.073Z` };
-_[2] = { 'description': `buy cigarettesl`, 'done': `false`, '_id': `09d34a8d-2d9f-421b-b75a-e88319c27433`, '_createdOn': `2021-09-22T15:13:42.836Z` };
